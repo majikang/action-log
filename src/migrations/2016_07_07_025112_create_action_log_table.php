5bc98d2a7233cb7f8523c4a45c37cd6daa65fe32
majikang <10307440@qq.com>
2016-09-30 16:46:26 +0800

    修改action_log迁移文件名错误

--- src/migrations/2016_07_07_025112_create_action_log_table.php
+++ src/migrations/2016_07_07_025112_create_action_log_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrateActionLogTable extends Migration
+class CreateActionLogTable extends Migration
 {
     /**
      * Run the migrations.

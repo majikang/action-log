3d9ca17e0d4611611c39f82ca352a4741a077dc3
YIHONG <macroyep@gmail.com>
2017-03-13 20:58:55 +0800

    移除seeder异常:Undefined index: HTTP_USER_AGENT， 添加 表字段 nullable()

--- src/Repositories/ActionLogRepository.php
+++ src/Repositories/ActionLogRepository.php
@@ -2,8 +2,15 @@
 namespace majikang\ActionLog\Repositories;
 
 use majikang\ActionLog\Services\clientService;
-class ActionLogRepository {
 
+class ActionLogRepository
+{
+
+
+    static function getHttpUserAgent()
+    {
+        return isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+    }
 
     /**
      * 记录用户操作日志
@@ -12,34 +19,34 @@ class ActionLogRepository {
      * @param ActionLog $actionLog
      * @return bool
      */
-    public function createActionLog($type,$content)
+    public function createActionLog($type, $content)
     {
-    	$actionLog = new \majikang\ActionLog\Models\ActionLog();
-    	if(auth()->check()){
-    		$actionLog->uid      = auth()->user()->id;
-    		$actionLog->username = auth()->user()->name;
-    	}else{
-    		$actionLog->uid  = 0;
-    		$actionLog->username ="访客";
-    	}
-         //请求操作类型
-        $actionLog->type   = $type ? $type:'';
-       	//系统版本
-        $actionLog->system = clientService::getPlatForm($_SERVER['HTTP_USER_AGENT'],true) ? clientService::getPlatForm($_SERVER['HTTP_USER_AGENT'],true):'';
+        $actionLog = new \majikang\ActionLog\Models\ActionLog();
+        if (auth()->check()) {
+            $actionLog->uid = auth()->user()->id;
+            $actionLog->username = auth()->user()->name;
+        } else {
+            $actionLog->uid = 0;
+            $actionLog->username = "访客";
+        }
+        //请求操作类型
+        $actionLog->type = $type ? $type : '';
+        //系统版本
+        $actionLog->system = clientService::getPlatForm(self::getHttpUserAgent(), true) ? clientService::getPlatForm(self::getHttpUserAgent(), true) : '';
         //请求的ip
-        $actionLog->ip     = request()->getClientIp() ? request()->getClientIp():'';
+        $actionLog->ip = request()->getClientIp() ? request()->getClientIp() : '';
 
         $region = clientService::getRegionFromIp(request()->getClientIp());
         //国家
-        $actionLog->country  = $region['country'] ? $region['country'] : '';
+        $actionLog->country = $region['country'] ? $region['country'] : '';
         //省份
         $actionLog->province = $region['province'] ? $region['province'] : '';
         //城市
-        $actionLog->city     = $region['city'] ? $region['city'] : '';
+        $actionLog->city = $region['city'] ? $region['city'] : '';
         //请求url
-        $actionLog->url      = request()->getRequestUri() ? request()->getRequestUri():'';
+        $actionLog->url = request()->getRequestUri() ? request()->getRequestUri() : '';
         //请求内容
-        $actionLog->content  = $content ? $content:'';
+        $actionLog->content = $content ? $content : '';
         $res = $actionLog->save();
 
         return $res;
@@ -52,46 +59,46 @@ class ActionLogRepository {
      * @param ActionLog $actionLog
      * @return bool
      */
-    public function apiLog($type,$content,$result)
+    public function apiLog($type, $content, $result)
     {
         $actionLog = new \majikang\ActionLog\Models\ActionLog();
-        if(auth()->check()){
+        if (auth()->check()) {
             $actionLog->uid = auth()->user()->id;
             $actionLog->username = auth()->user()->name;
-        }else{
-            $actionLog->uid=0;
-            $actionLog->username ="访客";
+        } else {
+            $actionLog->uid = 0;
+            $actionLog->username = "访客";
         }
 
-         //请求操作类型
-        $actionLog->type     = $type ? $type:'';
+        //请求操作类型
+        $actionLog->type = $type ? $type : '';
         //系统版本
-        $actionLog->system   = clientService::getPlatForm($_SERVER['HTTP_USER_AGENT'],true) ? clientService::getPlatForm($_SERVER['HTTP_USER_AGENT'],true):'';
+        $actionLog->system = clientService::getPlatForm(self::getHttpUserAgent(), true) ? clientService::getPlatForm(self::getHttpUserAgent(), true) : '';
         //请求的ip
-        $actionLog->ip       = request()->getClientIp() ? request()->getClientIp():'';
+        $actionLog->ip = request()->getClientIp() ? request()->getClientIp() : '';
 
 
         $region = clientService::getRegionFromIp(request()->getClientIp());
 
         //国家
-        $actionLog->country  = $region['country'] ? $region['country'] : '';
+        $actionLog->country = $region['country'] ? $region['country'] : '';
         //省份
         $actionLog->province = $region['province'] ? $region['province'] : '';
         //城市
-        $actionLog->city     = $region['city'] ? $region['city'] : '';
+        $actionLog->city = $region['city'] ? $region['city'] : '';
         //请求url
-        $actionLog->url      = request()->getRequestUri()?request()->getRequestUri():'';
+        $actionLog->url = request()->getRequestUri() ? request()->getRequestUri() : '';
         //请求内容
-        $actionLog->content  = $content ? $content:'';
+        $actionLog->content = $content ? $content : '';
 
         //终端型号
-        $actionLog->TermType  = request()->TermType ? request()->TermType:'';
+        $actionLog->TermType = request()->TermType ? request()->TermType : '';
         //终端版本号
-        $actionLog->TermVer   = request()->TermVer ? request()->TermVer:'';
+        $actionLog->TermVer = request()->TermVer ? request()->TermVer : '';
         //IMEI号
-        $actionLog->TermIMEI  = request()->TermIMEI ? request()->TermIMEI:'';
+        $actionLog->TermIMEI = request()->TermIMEI ? request()->TermIMEI : '';
         //终端ID号
-        $actionLog->TermID    = request()->TermID ? request()->TermID:'';
+        $actionLog->TermID = request()->TermID ? request()->TermID : '';
 
         //请求结果
         $actionLog->result = $result;
